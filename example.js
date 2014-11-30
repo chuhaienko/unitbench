4f168cfe100d307fe81382825b569f9f69bb6a17
Yuriy Chugaenko <kot@murkot.net>
2014-11-30 22:05:47 +0200

    v 0.1.1

--- example.js
+++ example.js
@@ -2,5 +2,9 @@ var Bench = require('./index.js');
 
 var b1 = new Bench();
 
-b1.start('1');
-b1.end('1');
+b1.start();
+for (var i = 0; i < 10000000; i++) {
+        var x = Math.random();
+}
+b1.end();
+console.log(b1.delta());
--- index.js
+++ index.js
@@ -4,20 +4,44 @@ var instance;
 function UnitBench()
 {
     this.memory = {start: {}, end: {}};
-    this.time = {start: 0, end: 0};
+    this.time = {};
+
+    this.calculated = {};
 }
 
 
 UnitBench.prototype.start = function () {
-
+    this.calculated = {};
+    this.time.start = process.hrtime();
 };
 
 UnitBench.prototype.end = function () {
-
+    this.time.end = process.hrtime();
 };
 
-UnitBench.prototype.data = function () {
+UnitBench.prototype.delta = function () {
+    if(!this.calculated.done) {
+        this.calculated.done = true;
 
+        this.calculated = {
+            time: Number(this.time.end[0] + '.' + this.time.end[1]) - Number(this.time.start[0] + '.' + this.time.start[1])
+        };
+    }
+    return this.calculated;
+};
+
+UnitBench.prototype.compare = function (obj) {
+    //todo: =
+    var outObj = {};
+    if(obj instanceof UnitBench) {
+        outObj = {
+            time: this.delta().time - obj.delta().time
+        };
+    } else {
+        outObj = {
+            time: this.delta().time
+        };
+    }
 };
 
 UnitBench.prototype.toConsole = function () {
