bafb983f474b7826383e917b242fe9892717caa9
Yuriy Chugaenko <kot@murkot.net>
2014-11-30 18:03:07 +0200

    init

---
+++ example.js
@@ -0,0 +1,6 @@
+var Bench = require('./index.js');
+
+var b1 = new Bench();
+
+b1.start('1');
+b1.end('1');
---
+++ index.js
@@ -0,0 +1,27 @@
+
+var instance;
+
+function UnitBench()
+{
+    this.memory = {start: {}, end: {}};
+    this.time = {start: 0, end: 0};
+}
+
+
+UnitBench.prototype.start = function () {
+
+};
+
+UnitBench.prototype.end = function () {
+
+};
+
+UnitBench.prototype.data = function () {
+
+};
+
+UnitBench.prototype.toConsole = function () {
+
+};
+
+module.exports = UnitBench;
